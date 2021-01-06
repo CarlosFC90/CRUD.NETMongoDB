af2c660deb1456245383e1865f9edf30086e9d10
Carlos Cabral <carlosfc.631@gmail.com>
2021-01-06 11:09:56 -0300

    API terminada

---
+++ Models/Beer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MVCMongoDB.Models
+{
+    public class Beer
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+        [BsonElement("name")]
+        public string Name { get; set; }
+        [BsonElement("alcohol")]
+        public decimal Alcohol { get; set; }
+    }
+}
